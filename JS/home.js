cdb190675c95b7eba762d357016373e9e982af8c
VanHuyen193 <132363333+VanHuyen193@users.noreply.github.com>
2024-06-03 16:50:57 +0700

    Slide

---
+++ JS/home.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const images = [
+        '/Image/HomeImages/banner.jpg',
+        '/Image/HomeImages/banner2.jpg'
+    ];
+    let currentIndex = 0;
+    const header = document.querySelector('.header');
+
+    function changeBackground() {
+        currentIndex = (currentIndex + 1) % images.length;
+        header.style.backgroundImage = `url(${images[currentIndex]})`;
+    }
+
+    setInterval(changeBackground, 3000); // Thay đổi hình ảnh mỗi 5 giây
+});
